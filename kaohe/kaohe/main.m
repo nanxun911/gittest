5022f4fffdb51b04468d616a7b6bc786e6b0f394
nanxun911 <3073623804@qq.com>
2024-05-21 20:28:40 +0800

    try

--- kaohe/kaohe/main.m
+++ kaohe/kaohe/main.m
@@ -14,14 +14,7 @@
 #import "calssA.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString* str1 = @"hello";
-        NSString* str2 = [NSString stringWithFormat:@"helloworld"];
-        NSString* str3 = [NSString stringWithFormat:@"神"];
-        NSString* str4 = [NSString stringWithFormat:@"hello"];
-        NSLog(@"%lu, %@", [str1 retainCount], [str1 class]);
-        NSLog(@"%lu, %@", [str2 retainCount], [str2 class]);
-        NSLog(@"%lu, %@", [str3 retainCount], [str3 class]);
-        NSLog(@"%lu, %@", [str4 retainCount], [str4 class]);
+       
         
     }
     return 0;
